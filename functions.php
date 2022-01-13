d64479de39bd4ce1de70c64846be68d4f4dff8a3
MalingaPeiris <63008840+MalingaPeiris@users.noreply.github.com>
2022-01-13 15:44:12 +0530

    bootstrap

--- functions.php
+++ functions.php
@@ -6,11 +6,21 @@
  */
 
 function aquila_enqueue_scripts(){
-    wp_register_style('style-css', get_stylesheet_uri(), [], filemtime( get_template_directory(). '/style.css' ), 'all' );
-    wp_register_script('main-js', get_template_directory_uri(). '/assets/main.js', [], filemtime( get_template_directory(). '/assets/main.js' ), true );
 
-    wp_enqueue_script( 'style-css');
+    //register styles
+    wp_register_style('style-css', get_stylesheet_uri(), [], filemtime( get_template_directory(). '/style.css' ), 'all' );
+    wp_register_style('bootstrap-css', get_template_directory_uri(). './assets/src/library/css/bootstrap.min.css', [], false , 'all' );
+
+    //register scripts
+    wp_register_script('main-js', get_template_directory_uri(). '/assets/main.js', [], filemtime( get_template_directory(). '/assets/main.js' ), true );
+    wp_register_script('bootstrap-js', get_template_directory_uri(). '/assets/src/library/js/bootstrap.min.js', ['jquery'], false, true );
+
+    //enqueue styles
+    wp_enqueue_style( 'style-css');
+    wp_enqueue_style( 'bootstrap-css');
+    //enqueue scripts
     wp_enqueue_script('main-js');
+    wp_enqueue_script('bootstrap-js');
 
 
 
