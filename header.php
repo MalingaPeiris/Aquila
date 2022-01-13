2fa53a5a7f8e85d90dfab5cdb4e26fb4a4547883
MalingaPeiris <63008840+MalingaPeiris@users.noreply.github.com>
2022-01-13 14:29:12 +0530

    wp_body_open

--- header.php
+++ header.php
@@ -14,5 +14,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php wp_head(); ?>
 </head>
-<body>
+<body <?php body_class( 'hello-class' )?> </body>>
+
+<?php wp_body_open(); ?>
+
     <header>Header</header>
