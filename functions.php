312f1780102ecd16f7b8bc8b9d680bf440cf097b
MalingaPeiris <63008840+MalingaPeiris@users.noreply.github.com>
2022-01-13 14:54:26 +0530

    enqueue stylesheet

--- functions.php
+++ functions.php
@@ -4,3 +4,11 @@
  * 
  * @package aquila
  */
+
+function aquila_enqueue_scripts(){
+    wp_enqueue_style('stylesheet', get_stylesheet_uri(), [], filemtime( get_template_directory(). '/style.css' ), 'all' );
+
+}
+add_action('wp_enqueue_scripts', 'aquila_enqueue_scripts')
+
+ ?>
--- header.php
+++ header.php
@@ -14,7 +14,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php wp_head(); ?>
 </head>
-<body <?php body_class( 'hello-class' )?> </body>>
+<body>
 
 <?php wp_body_open(); ?>
 
