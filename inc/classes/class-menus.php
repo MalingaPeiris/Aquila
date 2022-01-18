a9ee61c57be0fbd63971d9b984cf8ba8e4bb5612
MalingaPeiris <63008840+MalingaPeiris@users.noreply.github.com>
2022-01-18 17:12:09 +0530

    dunamic nav bar completed

--- inc/classes/class-menus.php
+++ inc/classes/class-menus.php
@@ -31,5 +31,28 @@
         ]);
     }
 
+    public function get_menu_id($location){
+        //get all thye locations
+        $locations=get_nav_menu_locations();
+        
+        //get object is by location
+        $menu_id=$locations[$location];
+
+        return ! empty($menu_id) ? $menu_id : '';
+    }
+
+    public function get_child_menu_items($menu_array, $parent_id) {
+        $child_menus=[];
+
+        if(! empty($menu_array)&& is_array($menu_array)){
+            foreach($menu_array as $menu){
+                if (intval($menu ->menu_item_parent)=== $parent_id) {
+                    array_push($child_menus, $menu);
+                }
+            }
+        }
+
+    }
+
     
  }
