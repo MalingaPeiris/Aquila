5df23bc179be1038d4e536090b67d467bacc298d
MalingaPeiris <63008840+MalingaPeiris@users.noreply.github.com>
2022-01-18 09:07:24 +0530

    singleton design pattern

--- functions.php
+++ functions.php
@@ -5,6 +5,12 @@
  * @package aquila
  */
 
+if(!defined('AQUILA_DIR_PATH')){
+    define('AQUILA_DIR_PATH', untrailingslashit(get_template_directory()));
+}
+
+ require_once AQUILA_DIR_PATH. '/inc/helpers/autoloader.php';
+
 function aquila_enqueue_scripts(){
 
     //register styles
---
+++ inc/traits/trait-singleton.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace AQUILA_THEME\Inc\Traits;
+
+trait singleton{
+    public function __construct(){
+
+    }
+    public function clone(){
+
+    }
+    fnila public static function get_instance(){
+        static $instance =[];
+         $called_class = get_called_class();
+
+         if(!isset($instance[$called_class])){
+             $instance[$called_class] =new $called_class();
+
+             do_action(sprintf('aquila_theme_singleton_init%s',$called_class));
+         }
+
+         return $instance[$called_class];
+    }
+}
