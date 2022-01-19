a0ba75ef02632af315fea8115aa40fd2a20e7c78
MalingaPeiris <63008840+MalingaPeiris@users.noreply.github.com>
2022-01-19 10:35:40 +0530

    the_post

--- index.php
+++ index.php
@@ -7,8 +7,24 @@
 get_header();
 ?>
 
-    <div class="content">
-        <?php esc_html_e('Content Blog', 'aquila'); ?>
+    <div id="primary">
+        <main id="main" class="site-main mt-5" role="main">
+            <?php
+            if (have_posts()) {
+            ?>
+            <div class="container">
+                <?php
+                    while(have_posts()) : the_post();
+                    the_title();
+                    the_content();
+            endwhile;
+                
+                ?>
+            </div>
+            <?php
+            }
+            ?>
+        </main>
     </div>
   
 <?php
--- template-parts/header/nav.php
+++ template-parts/header/nav.php
@@ -13,6 +13,7 @@
  ?>
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container">
   <?php
   if(function_exists('the_custom_logo')){
     the_custom_logo();
@@ -77,6 +78,7 @@
       <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
       <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
     </form>
+  </div>
   </div>
 </nav>
 
