90a5d4a4b4628db3c529bdf7dff9aca368a81e72
MalingaPeiris <63008840+MalingaPeiris@users.noreply.github.com>
2022-01-19 12:01:20 +0530

    page title

--- index.php
+++ index.php
@@ -13,7 +13,18 @@ get_header();
             if (have_posts()) {
             ?>
             <div class="container">
+
+            <?php
+            if ( is_home() && ! is_front_page() ) {
+                ?>
+                <header class="mb-5">
+                    <h1 class="page-title ">
+                        <?php single_post_title(); ?>
+                    </h1>
+                </header>
                 <?php
+            }
+            
                     while(have_posts()) : the_post();
                     the_title();
                     the_content();
