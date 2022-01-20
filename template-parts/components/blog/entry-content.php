4cef83d0beb186567329814ca2623276282505db
MalingaPeiris <63008840+MalingaPeiris@users.noreply.github.com>
2022-01-20 15:26:16 +0530

    pagination

--- template-parts/components/blog/entry-content.php
+++ template-parts/components/blog/entry-content.php
@@ -17,16 +17,29 @@
                     [
                         'span' => [
                             'class' => []
-                                    ]
+                        ]
                     ]
-                        ),
-                        the_title('<span calss="scren-reader-text">', '</span>', false)
+                ),
+                the_title('<span calss="scren-reader-text">', '</span>', false)
             )
         );
-    }else{
+
+        //pagination
+        wp_link_pages(
+            [
+                'before' => '<div class="page-links">' . esc_html__('Pages:', 'aquila'),
+                'after'  => '</div>',
+            ]
+        );
+
+    } else {
         //if it is not a single page post
         aquila_the_excerpt(150);
         echo aquila_excerpt_more();
     }
+
+    
+
+
     ?>
 </div>
