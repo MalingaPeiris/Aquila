bc14d891f43e444c640c8f6acabe8d96e5ce2e49
MalingaPeiris <63008840+MalingaPeiris@users.noreply.github.com>
2022-01-20 13:26:26 +0530

    post date and time

--- inc/helpers/template-tags.php
+++ inc/helpers/template-tags.php
@@ -35,3 +35,29 @@ function the_post_custom_thumbnail($post_id, $size, $additional_attributes = [])
 {
     echo get_the_post_custom_thumbnail($post_id, $size = 'featured-thumbnail', $additional_attributes);
 }
+
+function aquila_posted_on()
+{
+    $time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
+
+
+    //post is modified
+    if (get_the_time('U') !== get_the_modified_time('U')) {
+        $time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
+    }
+
+    $time_string = sprintf(
+        $time_string,
+        esc_attr(get_the_date(DATE_W3C)),
+        esc_attr(get_the_date()),
+        esc_attr(get_the_modified_date(DATE_W3C)),
+        esc_attr(get_the_modified_date())
+    );
+
+    $posted_on = sprintf(
+        esc_html_x('Posted on %s', 'post date', 'aquila'),
+        '<a href="' . esc_url(get_permalink()) . '" rel="author">' . $time_string . '</a>'
+    );
+
+    echo '<span class="posted-one tesxt-secandary">' . $posted_on . '</span>';
+}
---
+++ template-parts/components/blog/entry-meta.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * template for entry meta data\
+ * 
+ * @package aquila
+ */
+?>
+
+<div class="entry-meta mb-3">
+    <?php aquila_posted_on(); ?>
+</div>
