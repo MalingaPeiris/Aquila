bbdbc6965a0cb62e0b5114a6a09d321248ae913d
MalingaPeiris <63008840+MalingaPeiris@users.noreply.github.com>
2022-01-21 14:50:35 +0530

    webpack rules

--- assets/src/webpack.config.js
+++ assets/src/webpack.config.js
@@ -1,19 +1,51 @@
 const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-const JS_DIR = path.resolve(__dirname, '/src/js');
-const IMG_DIR = path.resolve(__dirname, '/src/img');
-const BUILD_DIR= path.resolve( __dirname, 'build');
+const JS_DIR = path.resolve(__dirname, "/src/js");
+const IMG_DIR = path.resolve(__dirname, "/src/img");
+const BUILD_DIR = path.resolve(__dirname, "build");
 
 const entry = {
-    main: JS_DIR + '/main.js',
-    single: JS_DIR + '/single.js',
+  main: JS_DIR + "/main.js",
+  single: JS_DIR + "/single.js",
 };
 const output = {
-    path: BUILD_DIR,
-    filename: 'js/[name].js'
+  path: BUILD_DIR,
+  filename: "js/[name].js",
 };
+const rules = [
+  {
+    test: /\.js$/,
+    include: [JS_DIR],
+    exclude: /node_modules/,
+    use: "babel-loader",
+  },
+  {
+    test: /\.scss$/,
+    exclude: /node_modules/,
+    use: [MiniCssExtractPlugin.loader, "css-loader"],
+  },
+  {
+    test: /\.(png|jpg|svg|jpeg|gif|ico)$/,
+    include: [JS_DIR],
+    exclude: /node_modules/,
+    use: [
+      {
+        loader: "file-loader",
+        options: {
+          name: "[path][name].[ext]",
+          publicPath: "production" === process.env.NODE_ENV ? "../" : "../../",
+        },
+      },
+    ],
+  },
+];
 
 module.exports = (env, argv) => ({
-    entry: entry,
-    output: output,
+  entry: entry,
+  output: output,
+  devtool: "source-map",
+  module: {
+    rules: rules,
+  },
 });
