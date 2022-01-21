5a3a1e8cc7be75e3a9e15849f4dc19261f0bcc3d
MalingaPeiris <63008840+MalingaPeiris@users.noreply.github.com>
2022-01-21 10:27:43 +0530

    show the side bar

--- footer.php
+++ footer.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * footer template
  *
@@ -7,11 +8,23 @@
 
 ?>
 
-<footer>Footer</footer>
+<footer>
+    <h3>Footer</h3>
+    <?php
+    if (is_active_sidebar('sidebar-2')) {
+    ?>
+        <aside>
+            <?php dynamic_sidebar('sidebar-2'); ?>
+        </aside>
+    <?php
+    }
+    ?>
+</footer>
 
 </div>
 </div>
 
 <?php wp_footer(); ?>
 </body>
+
 </html>
---
+++ sidebar.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * the sidebar contaning the main widget AREA
+ * 
+ * @package aquila
+ */
+
+ ?>
+
+ <aside id="secondary" role="complementary">
+     <?php
+     dynamic_sidebar('sidebar-1');
+     ?>
+ </aside>
