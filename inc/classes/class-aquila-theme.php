843c96e01343f706e741893ac84007459d5f1e0c
MalingaPeiris <63008840+MalingaPeiris@users.noreply.github.com>
2022-01-21 09:50:56 +0530

    register sidebar

--- inc/classes/class-aquila-theme.php
+++ inc/classes/class-aquila-theme.php
@@ -19,6 +19,7 @@ class AQUILA_THEME
         Assets::get_instance();
         Menus::get_instance();
         Meta_Boxes::get_instance();
+        Sidebars::get_instance();
 
         $this->setup_hooks();
     }
---
+++ inc/classes/class-sidebars.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ *
+ * Theme Sidebars
+ *
+ * @package aquila
+ */
+
+namespace Aquila_Theme\Inc;
+
+use AQUILA_THEME\Inc\Traits\Singleton;
+
+class Sidebars
+{
+    use Singleton;
+
+    protected function __construct()
+    {
+        //load classes
+
+        $this->setup_hooks();
+    }
+
+    protected function setup_hooks()
+    {
+        //actions
+        add_action('widgets_init', [$this, 'register_sidebars']);
+    }
+
+    public function register_sidebars(){
+        register_sidebar([
+            'name' => __( 'Sidebar', 'aquila'),
+            'id' => 'sidebar-1',
+            'description' => __( 'Main sidebar', 'aquila'),
+            'before_widget' => '<div id="%1$s" class="widget widget-sidebar %2$s">',
+            'after_widget' => '</div>',
+            'before_title' => '<h3 class="widget-title">',
+            'after_title' => '</h3>',
+        ]);
+
+        register_sidebar([
+            'name' => __( 'Footer', 'aquila'),
+            'id' => 'sidebar-2',
+            'description' => __( 'Footer sidebar', 'aquila'),
+            'before_widget' => '<div id="%1$s" class="widget widget-footer cell column %2$s">',
+            'after_widget' => '</div>',
+            'before_title' => '<h3 class="widget-title">',
+            'after_title' => '</h3>',
+        ]);
+    }
+
+    
+}
