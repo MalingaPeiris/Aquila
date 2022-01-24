84130db818f8e0ddcbe0f5ef17b9da3ecab5a2f2
MalingaPeiris <63008840+MalingaPeiris@users.noreply.github.com>
2022-01-24 14:47:56 +0530

    import fonts

--- inc/classes/class-assets.php
+++ inc/classes/class-assets.php
@@ -31,14 +31,11 @@ class Assets
     public function register_styles()
     {
         //register styles
-        wp_register_style('style-css', get_stylesheet_uri(), [], filemtime(AQUILA_DIR_PATH . '/style.css'), 'all');
         wp_register_style('bootstrap-css', AQUILA_DIR_URI . '/assets/src/library/css/bootstrap.min.css', false, 'all');
         wp_register_style('main-css', AQUILA_BUILD_CSS_URI . '/main.css', ['bootstrap-css'], filemtime(AQUILA_BUILD_CSS_DIR_PATH . '/main.css'), 'all');    
-        wp_enqueue_style('fonts-css', get_template_directory_uri() . '/assets/src/library/fonts/fonts.css', [], false, 'all');
-
+       
         //enqueue styles
         wp_enqueue_style('bootstrap-css');
-        wp_enqueue_style('style-css');
         wp_enqueue_style('main-css');
 
         
--- inc/helpers/template-tags.php
+++ inc/helpers/template-tags.php
@@ -96,7 +96,7 @@ function aquila_excerpt_more($more = '')
 {
     if (!is_single()) {
         $more = sprintf(
-            '<button class="mt-4 btn btn-info"><a class="aquila-read-more text-white" href="%1$s">%2$s</a></button>',
+            '<a class="aquila-read-more text-white" href="%1$s"><button class="mt-4 btn btn-info">%2$s</button></a>',
             get_permalink(get_the_ID()),
             __('Read more', 'aquila')
         );
